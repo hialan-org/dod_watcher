d802b21a55406cf1b3aad46ee964e65676f0a87a
AlperOzdamar <alper.ozdamar@gmail.com>
2020-02-17 19:41:13 -0800

    micronaut & hibernate integration

---
+++ src/main/java/usf/sdlc/model/User.java
@@ -0,0 +1,48 @@
+package usf.sdlc.model;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    private int userId;
+
+    private String email;
+
+    private Timestamp createdDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+
+}
