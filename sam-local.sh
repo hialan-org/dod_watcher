7021cd23d4f6a5c2bc3f9a367eec5a33c4627193
Hiep Bui <phuhiep11395@outlook.com>
2020-02-19 08:13:56 -0800

    Try with new architecture without Graal

--- sam-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build . -t dod
-mkdir -p build
-docker run --rm --entrypoint cat dod  /home/application/function.zip > build/function.zip
-
-sam local start-api -t sam.yaml -p 3000
-
--- src/main/java/usf/sdlc/Application.java
+++ src/main/java/usf/sdlc/Application.java
@@ -3,7 +3,6 @@ package usf.sdlc;
 import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 
-@TypeHint(typeNames = "org.postgresql.Driver.class")
 public class Application {
 
     public static void main(String[] args) {
