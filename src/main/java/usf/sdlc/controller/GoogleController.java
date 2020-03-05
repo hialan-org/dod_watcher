26e5bee3648e834a816a8da94f57fb73f250ac6c
Hiep Bui <phuhiep11395@outlook.com>
2020-03-05 12:27:32 -0800

    Try with cors header

--- src/main/java/usf/sdlc/controller/GoogleController.java
+++ src/main/java/usf/sdlc/controller/GoogleController.java
@@ -10,6 +10,8 @@ import usf.sdlc.model.User;
 import usf.sdlc.service.UserService;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller()
 public class GoogleController {
@@ -29,6 +31,10 @@ public class GoogleController {
         if(user==null){
             return HttpResponse.unauthorized();
         }
-        return HttpResponse.ok(user).header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        Map<CharSequence, CharSequence> headers = new HashMap<>();
+        headers.put(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        headers.put(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        return HttpResponse.ok(user).headers(headers);
+//        return HttpResponse.ok(user).header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
     }
 }
