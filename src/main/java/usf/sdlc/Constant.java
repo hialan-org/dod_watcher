d2d1897b8e5ec191a20860debbe5f55c021e377e
Hiep Bui <phuhiep11395@outlook.com>
2020-03-17 14:08:33 -0700

    Change expo client id

--- src/main/java/usf/sdlc/Constant.java
+++ src/main/java/usf/sdlc/Constant.java
@@ -2,7 +2,8 @@ package usf.sdlc;
 
 public class Constant {
     public static final String GOOGLE_CLIENT_ID = "152090196286-pkjcq0hb22e0s6c89skktrvqscjo1ok5.apps.googleusercontent.com";
-    public static final String EXPO_CLIENT_ID = "603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com";
+//    public static final String EXPO_CLIENT_ID = "603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com";
+    public static final String EXPO_CLIENT_ID = "603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com";
     public static final String ROLE_ADMIN = "admin";
     public static final String ROLE_USER = "user";
 }
