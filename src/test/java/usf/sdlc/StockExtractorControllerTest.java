44d26cd5d61dcbc82fa81a702dcb318e5dee8bc4
AlperOzdamar <alper.ozdamar@gmail.com>
2020-05-10 00:41:48 -0700

    Modifying and adding tests for StockExtractorController.  (StockExtractorControllerTest)

--- src/test/java/usf/sdlc/StockExtractorControllerTest.java
+++ src/test/java/usf/sdlc/StockExtractorControllerTest.java
@@ -12,12 +12,15 @@ import usf.sdlc.form.StockForm;
 import usf.sdlc.service.StockExtractorService;
 
 import javax.inject.Inject;
-
 import java.lang.reflect.Type;
-import java.util.*;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @MicronautTest
 public class StockExtractorControllerTest {
@@ -31,27 +34,35 @@ public class StockExtractorControllerTest {
     @Inject
     StockExtractorService stockExtractorService;
 
-//    @Test
-//    void testGetStockDetailsFromOutside() {
-//        System.out.println("Starting testFetchAllFromStockTable");
-//        // forming uri to hit IEX endpoint // todo - get token from github secret
-//        String symStr = "dow,aapl";
-//        String uri = "https://cloud.iexapis.com/v1/stock/market/batch?types=quote,stats&symbols="+symStr+"&token=pk_76512460ba7a434eb1aff6f1e40f0f1a";
-//        HttpRequest<String> request = HttpRequest.GET(uri);
-//        String body = client.toBlocking().retrieve(request);
-////        Publisher<String> body = client.retrieve(request);
-//        //// converting HTTP response to java object
-//        Type type = new TypeToken<HashMap<String, StockForm>>(){}.getType();
-//        Gson gson = new Gson();
-//        //System.out.println("BODY : "+ body);
-//        HashMap<String, StockForm> stockDetails = gson.fromJson(String.valueOf(body), type);
-//
-//        assertEquals(2, stockDetails.size());
-//    }
+    @Test
+    void testGetStockDetailsFromOutside() {
+        System.out.println("Starting testGetStockDetailsFromOutside");
+        // forming uri to hit IEX endpoint // todo - get token from github secret
+        String symStr = "dow,aapl";
+        String uri = "https://cloud.iexapis.com/v1/stock/market/batch?types=quote,stats&symbols="+symStr+"&token=pk_76512460ba7a434eb1aff6f1e40f0f1a";
+        HttpRequest<String> request = HttpRequest.GET(uri);
+        String body = client.toBlocking().retrieve(request);
+
+        //// converting HTTP response to java object
+        Type type = new TypeToken<HashMap<String, StockForm>>(){}.getType();
+        Gson gson = new Gson();
+        //System.out.println("BODY : "+ body);
+        HashMap<String, StockForm> stockDetails = gson.fromJson(String.valueOf(body), type);
+
+        assertEquals(2, stockDetails.size());
+
+        assertEquals(null, stockDetails.get("dow"));
+        assertNotEquals(null, stockDetails.get("DOW"));
+        assertEquals("DOW", stockDetails.get("DOW").getQuote().getSymbol());
+        assertEquals(null, stockDetails.get("aapl"));
+        assertNotEquals(null, stockDetails.get("AAPL"));
+        assertEquals("AAPL", stockDetails.get("AAPL").getQuote().getSymbol());
+    }
 
 //    @Test
 //    void testFetch2() {
 //        System.out.println("Starting testFetch2");
+//        //We invoke the controller and retrieve the response.
 //        StockExtractorResponse response = client.toBlocking().retrieve(HttpRequest.GET("/run-extractor"), StockExtractorResponse.class);
 //        String  q = response.getMessage();
 //        System.out.println(q);
@@ -67,16 +78,17 @@ public class StockExtractorControllerTest {
 //        assertEquals(30, q.size());
 //    }
 
-//    @Test
-//    void testConvertToTimestamp() throws ParseException {
-//        String timeStr = "February 20, 2020";
-//        String[] timeStrArr = timeStr.split(" ");
-//        if (timeStrArr.length > 0) {
-//            timeStrArr[1] = timeStrArr[1].substring(0, timeStrArr[1].length()-1);
-//        }
-//        Date date = new SimpleDateFormat("yyyy-MMMM-dd").parse(timeStrArr[2]+"-"+timeStrArr[0]+"-"+timeStrArr[1]);
-//        Timestamp timestamp = new Timestamp(date.getTime());
-//        System.out.println(timestamp.toString());
-//
-//    }
+    @Test
+    void testConvertToTimestamp() throws ParseException {
+        String timeStr = "February 20, 2020";
+        String[] timeStrArr = timeStr.split(" ");
+        if (timeStrArr.length > 0) {
+            timeStrArr[1] = timeStrArr[1].substring(0, timeStrArr[1].length()-1);
+        }
+        Date date = new SimpleDateFormat("yyyy-MMMM-dd").parse(timeStrArr[2]+"-"+timeStrArr[0]+"-"+timeStrArr[1]);
+        Timestamp timestamp = new Timestamp(date.getTime());
+        System.out.println("date:"+date);
+        assertEquals("2020-02-20 00:00:00.0",timestamp.toString());
+
+    }
 }
